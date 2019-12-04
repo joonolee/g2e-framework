0e74c86a1ca5850681947c3263e8839b2557e4dd
JunHo Lee <joono_lee@naver.com>
2019-12-04 10:15:28 +0900

    최신 유틸 라이브러리 적용, 어플리케이션 정리, 빌드 파일 보강

--- src/main/java/kr/co/g2e/appname/AppnameApplication.java
+++ src/main/java/kr/co/g2e/appname/AppnameApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,16 +22,22 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import kr.co.g2e.utils.cache.Cache;
 import kr.co.g2e.utils.filter.RequestLoggingFilter;
 import kr.co.g2e.utils.util.Params;
 
 @SpringBootApplication
-@MapperScan("kr.co.g2e.appname")
+@MapperScan(basePackages = "kr.co.g2e.appname", annotationClass = Mapper.class)
 public class AppnameApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
+	/**
+	 * 스프링부트 메인
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AppnameApplication.class, args);
+		Cache.init(); // 캐시 초기화
 	}
 
 	/**
@@ -38,9 +45,21 @@ public class AppnameApplication extends SpringBootServletInitializer implements 
 	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		Cache.init(); // 캐시 초기화
 		return application.sources(AppnameApplication.class);
 	}
 
+	/**
+	 * 인덱스 페이지 설정
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+
+	//===================================================================
+	// 빈 객체 생성
+	//===================================================================
 	/**
 	 * 요청 로깅 필터 등록(헤더, 쿠키, 파라미터 정보, 실행시간 정보 조회)
 	 */
@@ -53,11 +72,28 @@ public class AppnameApplication extends SpringBootServletInitializer implements 
 	}
 
 	/**
+	 * 로그인 체크 필터 생성
+	 */
+	@Bean
+	public LoginCheckInterceptor loginCheckInterceptor() {
+		return new LoginCheckInterceptor();
+	}
+
+	/**
+	 * 권한 체크 필터 생성
+	 */
+	@Bean
+	public PermCheckInterceptor permCheckInterceptor() {
+		return new PermCheckInterceptor();
+	}
+
+	/**
 	 * 인터셉터 등록
 	 */
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoginCheckInterceptor()).addPathPatterns("/**/*.do"); // 로그인체크
+		registry.addInterceptor(loginCheckInterceptor()).addPathPatterns("/**/*.do"); // 로그인체크
+		registry.addInterceptor(permCheckInterceptor()).addPathPatterns("/**/*.do"); // 권한체크
 	}
 
 	/**
@@ -68,6 +104,9 @@ public class AppnameApplication extends SpringBootServletInitializer implements 
 		resolvers.add(new ParamsResolver()); // Params
 	}
 
+	//===================================================================
+	// 인터셉터 클래스 정의
+	//===================================================================
 	/**
 	 * 로그인 체크 인터셉터 정의
 	 */
@@ -82,7 +121,23 @@ public class AppnameApplication extends SpringBootServletInitializer implements 
 	}
 
 	/**
-	 * Params Argument 리졸버 정의
+	 * 권한 체크 인터셉터 정의
+	 */
+	public static class PermCheckInterceptor implements HandlerInterceptor {
+		private Logger logger = LoggerFactory.getLogger(getClass());
+
+		@Override
+		public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+			logger.debug("===================== 권한 체크 성공");
+			return true; // true 이면 계속 진행, false 이면 다음 호출 중단
+		}
+	}
+
+	//===================================================================
+	// 리졸버 클래스 정의
+	//===================================================================
+	/**
+	 * Params 인자 리졸버
 	 */
 	public static class ParamsResolver implements HandlerMethodArgumentResolver {
 		@Override
